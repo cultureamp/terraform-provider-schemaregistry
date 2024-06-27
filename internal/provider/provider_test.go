16368cbe8e75c41411f7c8abce40a8ffab1cfdab
dstrates <99311378+dstrates@users.noreply.github.com>
2024-06-27 19:00:14 +1000

    feat: add initial schema resource
    - Add initial schema resource
    - Update go modules
    - Minor updates to resource
    - Delete placeholder examples
    - Add initial resource schema test
    - Update provider and schema resource definition
    - Update example terraform files
    - Disable generate and test
    - Update example terraform files
    - Update resource error handling and tests

--- internal/provider/provider_test.go
+++ internal/provider/provider_test.go
@@ -1,51 +1,15 @@
 package provider
 
 import (
-	"testing"
-
 	"github.com/hashicorp/terraform-plugin-framework/providerserver"
 	tfprotov6 "github.com/hashicorp/terraform-plugin-go/tfprotov6"
-	"github.com/hashicorp/terraform-plugin-testing/helper/resource"
 )
 
 const (
 	testAccProviderVersion = "0.0.1"
-	testAccProviderType    = "schema_registry"
-
-	providerConfig = `
-	provider "schemaregistry" {
-	  schema_registry_url = "http://test-url"
-	  username            = "test-user"
-	  password            = "test-pass"
-	}
-	`
-
-	resourceConfig = `
-	resource "schemaregistry_schema" "test" {
-	  subject 			  = "test-subject"
-	  schema  			  = "{\"type\":\"record\",\"name\":\"Test\",\"fields\":[{\"name\":\"f1\",\"type\":\"string\"}]}"
-	  schema_type 		  = "avro"
-	  compatibility_level = "NONE"
-	}
-	`
+	testAccProviderType    = "schemaregistry"
 )
 
 var testAccProtoV6ProviderFactories = map[string]func() (tfprotov6.ProviderServer, error){
 	"schemaregistry": providerserver.NewProtocol6WithError(New(testAccProviderVersion)()),
-}
-
-func TestAccSchemaRegistryProvider(t *testing.T) {
-	resource.Test(t, resource.TestCase{
-		ProtoV6ProviderFactories: testAccProtoV6ProviderFactories,
-		Steps: []resource.TestStep{
-			{
-				Config: providerConfig + resourceConfig,
-				Check: resource.ComposeAggregateTestCheckFunc(
-					resource.TestCheckResourceAttr("schemaregistry_schema.test", "schema_registry_url", "https://schema-registry.kafka.usw2.dev-us.cultureamp.io"),
-					resource.TestCheckResourceAttr("schemaregistry_schema.test", "username", "test-user"),
-					resource.TestCheckResourceAttr("schemaregistry_schema.test", "password", "test-pass"),
-				),
-			},
-		},
-	})
 }
---
+++ internal/provider/resource_schema_test.go
@@ -0,0 +1,58 @@
+package provider
+
+import (
+	"fmt"
+	"testing"
+
+	"github.com/hashicorp/terraform-plugin-testing/helper/acctest"
+	"github.com/hashicorp/terraform-plugin-testing/helper/resource"
+)
+
+func TestAccSchemaResource_CreateReadImport(t *testing.T) {
+	rName := acctest.RandomWithPrefix("tf-acc-test")
+	resourceName := "schemaregistry_schema.test_01"
+
+	resource.ParallelTest(t, resource.TestCase{
+		ProtoV6ProviderFactories: testAccProtoV6ProviderFactories,
+		Steps: []resource.TestStep{
+			// Create and Read testing
+			{
+				Config: testAccSchemaRegistryConfig_base(rName),
+				Check: resource.ComposeAggregateTestCheckFunc(
+					// Verify schema attributes
+					resource.TestCheckResourceAttr(resourceName, "subject", rName),
+					resource.TestCheckResourceAttr(resourceName, "schema", "{\"type\":\"record\",\"name\":\"Test\",\"fields\":[{\"name\":\"f1\",\"type\":\"string\"}]}"),
+					resource.TestCheckResourceAttr(resourceName, "schema_type", "avro"),
+					resource.TestCheckResourceAttr(resourceName, "compatibility_level", "NONE"),
+					resource.TestCheckResourceAttrSet(resourceName, "schema_id"),
+					resource.TestCheckResourceAttrSet(resourceName, "version"),
+				),
+			},
+			// ImportState testing
+			{
+				ResourceName:      resourceName,
+				ImportState:       true,
+				ImportStateVerify: true,
+				// No attributes to ignore during import
+				ImportStateVerifyIgnore: []string{},
+			},
+		},
+	})
+}
+
+func testAccSchemaRegistryConfig_base(rName string) string {
+	return fmt.Sprintf(`
+provider "schemaregistry" {
+  schema_registry_url = "https://schema-registry.kafka.usw2.dev-us.cultureamp.io"
+  username            = "test-user"
+  password            = "test-pass"
+}
+
+resource "schemaregistry_schema" "test_01" {
+  subject              = "%s"
+  schema_type          = "avro"
+  compatibility_level  = "NONE"
+  schema               = "{\"type\":\"record\",\"name\":\"Test\",\"fields\":[{\"name\":\"f1\",\"type\":\"string\"}]}"
+}
+`, rName)
+}
