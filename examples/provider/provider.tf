e4694beb4f269f8191b54b1dc69b07c046efea93
dstrates <99311378+dstrates@users.noreply.github.com>
2024-07-01 12:32:20 +1000

    docs: update docs and examples

--- examples/provider/provider.tf
+++ examples/provider/provider.tf
@@ -1,3 +1,12 @@
+terraform {
+  required_providers {
+    schemaregistry = {
+      source = "local/schemaregistry"
+      version = "0.1.0"
+    }
+  }
+}
+
 provider "schemaregistry" {
   schema_registry_url = "localhost:8081"
   username            = "test-user"
